f303c2510fef3a93cc0a0e1bf951a65a63486e69
Daniel Braz <danielbrazlopes16@gmail.com>
2023-12-04 18:53:44 -0300

    feat(main): add type ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,15 +1,83 @@
-export class User {
-  id: string;
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { hash, compare } from 'bcryptjs';
+
+enum Active {
+  TRUE = 1,
+  FALSE = 0,
+}
+
+enum Local {
+  TRUE = 1,
+  FALSE = 0,
+}
+
+interface IProps {
   company_name: string;
   fantasy_name: string;
   cnpj: string;
+  local: Local;
+  opening_date: Date;
+  active: Active;
+  password: string;
+}
+
+@Entity()
+export class User {
+  constructor(props: IProps) {
+    Object.assign(this, props);
+  }
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  company_name: string;
+
+  @Column()
+  fantasy_name: string;
+
+  @Column()
+  cnpj: string;
+
+  @Column()
   local: number;
+
+  @Column({ type: 'datetime' })
   opening_date: string;
+
+  @Column()
   active: number;
+
+  password: string;
+
+  @Column()
   hash_password: string;
 
-  checkPassword(): boolean {
-    return false;
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  updated_at: Date;
+
+  async hashPassword(password: string): Promise<string> {
+    return await hash(password, 12);
+  }
+
+  async checkPassword(attempt: string): Promise<boolean> {
+    return await compare(attempt, this.password);
   }
 
   getUserSpecialties(): any[] {
