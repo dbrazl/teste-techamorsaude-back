c82dc8d0fd0573684fbe94d2ff0433ee12b4f2d4
Daniel Braz <danielbrazlopes16@gmail.com>
2023-12-07 00:29:36 -0300

    feat(main): create authenticate user unit test

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthenticateUserUseCase } from './use-cases/authenticate-user.use-case';
 import { AuthenticatedUserDto } from './dto/authenticated-user.dto';
 import { User } from '../users/entities/user.entity';
+import { LoginUserDto } from './dto/login-user.dto';
 
 const opening_date = new Date();
 
@@ -16,7 +17,7 @@ const user: User = new User({
   password: undefined,
 });
 
-const loginUserDto: AuthenticatedUserDto = {
+const authenticatedUserDto: AuthenticatedUserDto = {
   user,
   token:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
@@ -33,7 +34,7 @@ describe('AuthService', () => {
         {
           provide: AuthenticateUserUseCase,
           useValue: {
-            execute: jest.fn().mockResolvedValue(loginUserDto),
+            execute: jest.fn().mockResolvedValue(authenticatedUserDto),
           },
         },
       ],
@@ -47,5 +48,29 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('login', () => {
+    const loginUserDto: LoginUserDto = {
+      cnpj: '12345678912345',
+      password: '123456',
+    };
+
+    it('should called login', async () => {
+      await service.login(loginUserDto);
+      expect(authenticateUser.execute).toHaveBeenCalledWith(loginUserDto);
+    });
+
+    it('should login user successfully', async () => {
+      const result = await service.login(loginUserDto);
+      expect(result).toBe(authenticatedUserDto);
+    });
+
+    it('should throw a exception', () => {
+      jest
+        .spyOn(authenticateUser, 'execute')
+        .mockRejectedValueOnce(new Error());
+      expect(service.login(loginUserDto)).rejects.toThrow();
+    });
   });
 });
---
+++ src/auth/use-cases/authenticate-user.use-case.spec.ts
@@ -0,0 +1,128 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from '../auth.service';
+import { AuthenticateUserUseCase } from '../use-cases/authenticate-user.use-case';
+import { AuthenticatedUserDto } from '../dto/authenticated-user.dto';
+import { User } from '../../users/entities/user.entity';
+import { LoginUserDto } from '../dto/login-user.dto';
+import { UsersService } from '../../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { UsersModule } from '../../users/users.module';
+import { UnauthorizedException } from '@nestjs/common';
+
+const opening_date = new Date();
+
+const user: User = new User({
+  company_name: 'Company name',
+  fantasy_name: 'Fantasy name',
+  cnpj: '12345678912345',
+  local: 1,
+  active: 1,
+  opening_date,
+  password: undefined,
+});
+
+const userResponse: User = new User({
+  company_name: 'Company name',
+  fantasy_name: 'Fantasy name',
+  cnpj: '12345678912345',
+  local: 1,
+  active: 1,
+  opening_date,
+  password: undefined,
+});
+
+const token: string =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
+const authenticatedUserDto: AuthenticatedUserDto = {
+  user: userResponse,
+  token,
+};
+
+describe('AuthenticateUserUseCase', () => {
+  let useCase: AuthenticateUserUseCase;
+  let usersService: UsersService;
+  let jwtService: JwtService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthenticateUserUseCase,
+        {
+          provide: UsersService,
+          useValue: {
+            findOne: jest.fn().mockResolvedValue(user),
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn().mockReturnValue(token),
+          },
+        },
+      ],
+    }).compile();
+
+    useCase = module.get<AuthenticateUserUseCase>(AuthenticateUserUseCase);
+    usersService = module.get<UsersService>(UsersService);
+    jwtService = module.get<JwtService>(JwtService);
+  });
+
+  it('should be defined', () => {
+    expect(useCase).toBeDefined();
+  });
+
+  describe('execute', () => {
+    const loginUserDto: LoginUserDto = {
+      cnpj: '12345678912345',
+      password: '123456',
+    };
+
+    it('should called findOne', async () => {
+      jest.spyOn(user, 'checkPassword').mockResolvedValue(true);
+      await useCase.execute(loginUserDto);
+      expect(usersService.findOne).toHaveBeenCalledWith(loginUserDto.cnpj);
+    });
+
+    it('should called checkPassword', async () => {
+      await useCase.execute(loginUserDto);
+      expect(user.checkPassword).toHaveBeenCalledWith(loginUserDto.password);
+    });
+
+    it('should called sign', async () => {
+      jest.spyOn(user, 'checkPassword').mockResolvedValue(true);
+      await useCase.execute(loginUserDto);
+      expect(jwtService.sign).toHaveBeenCalledWith(userResponse);
+    });
+
+    it('should login user successfully', async () => {
+      jest.spyOn(user, 'checkPassword').mockResolvedValue(true);
+      const result = await useCase.execute(loginUserDto);
+      expect(result).toEqual(authenticatedUserDto);
+    });
+
+    it('should throw a exception when findOne fails', () => {
+      jest.spyOn(usersService, 'findOne').mockRejectedValueOnce(new Error());
+      expect(useCase.execute(loginUserDto)).rejects.toThrow();
+    });
+
+    it('should throw a exception when checkPassword fails', () => {
+      jest.spyOn(user, 'checkPassword').mockRejectedValueOnce(new Error());
+      expect(useCase.execute(loginUserDto)).rejects.toThrow();
+    });
+
+    it('should throw a UnauthorizedException because user not exist', () => {
+      jest.spyOn(usersService, 'findOne').mockResolvedValueOnce(null);
+      expect(useCase.execute(loginUserDto)).rejects.toThrow(
+        UnauthorizedException,
+      );
+    });
+
+    it('should throw a UnauthorizedException because password not match', () => {
+      jest.spyOn(user, 'checkPassword').mockResolvedValueOnce(false);
+      expect(useCase.execute(loginUserDto)).rejects.toThrow(
+        UnauthorizedException,
+      );
+    });
+  });
+});
